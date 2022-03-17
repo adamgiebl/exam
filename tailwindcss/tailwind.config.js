54b4d8fb5dc37dec8df957a58387bccf315ecc59
Santiago Donoso <sand@kea.dk>
2022-03-17 09:53:37 +0100

    ...

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge : ["../views/*.html"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "blue1": "#1DA1F2",
+        "blue2": "#2795D9",
+        "blue": "#EFF9FF",
+        "dark": "#657786",
+        "light": "#AAB8C2",
+        "lighter": "#E1E8ED",
+        "lightest": "#F5F8FA",
+      }         
+    },
   },
   plugins: [],
 }
